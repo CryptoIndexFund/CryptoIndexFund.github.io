d1d69ceb79e506e9b1c1af0d17ea3074129c1f5e
CryptoIndexFund <boomerang1986@aol.com>
2018-08-05 20:59:29 -0400

    Adds donation widgets

---
+++ js/donate.js
@@ -0,0 +1,10 @@
+!function(c){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.onload=c,t.src="//lab.subinsb.com/projects/francium/cryptodonate/widget.js";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}(function(){
+    Fr.loadCD("donate-btc", {
+        coin: "bitcoin",
+        address: "3NLXczbXEDA6EKS6a8d4BUd1ck9Eu1ASeW"
+    });
+    Fr.loadCD("donate-eth", {
+        coin: "ethereum",
+        address: "0xbC41Bc726929AF9c3B2558f339De59c14b87e4Dd"
+    });
+});
